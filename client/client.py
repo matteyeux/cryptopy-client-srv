9aec013d06f42a834314b37f6f523fd0e925c98c
matteyeux <mathieu.hautebas@gmail.com>
2017-12-14 09:58:37 +0000

    initial client file which sends a string

--- client/client.py
+++ client/client.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-
 import sys
 import os
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '127.0.0.1'
+port = 2345
+
+def serv_function(srv_ip, srv_port, msg2srv):
+    sock.connect((srv_ip, srv_port))
+    sock.send(msg2srv.encode())
 
 def usage(python_file):
+    print("usage:", python_file, "[message]")
 
 if __name__ == '__main__':
     argc = len(sys.argv)
     if argc != 2:
-        print
-    print(sys.argv[1])
-prout
-
-
+        usage(sys.argv[0])
+        sys.exit()
+    message = sys.argv[1]
+    serv_function("127.0.0.1", 2345, message)
